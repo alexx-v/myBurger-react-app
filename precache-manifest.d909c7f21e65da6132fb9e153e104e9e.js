b4e45df904c0fac1f17293c7e86b2f4429dc47ae
alexx-v <aleksandr1vlk@gmail.com>
2020-08-16 20:05:24 +0300

    Updates

---
+++ precache-manifest.d909c7f21e65da6132fb9e153e104e9e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b548baa0d3472df42e60e994f5a7565f",
+    "url": "/myBurger-react-app/index.html"
+  },
+  {
+    "revision": "0f6d8bdb0957073f5290",
+    "url": "/myBurger-react-app/static/css/3.22920352.chunk.css"
+  },
+  {
+    "revision": "95076e3ce860fd4f9862",
+    "url": "/myBurger-react-app/static/css/4.7ab15faf.chunk.css"
+  },
+  {
+    "revision": "e6134b2e58ec4953b418",
+    "url": "/myBurger-react-app/static/css/5.e8a10254.chunk.css"
+  },
+  {
+    "revision": "4095663e76a15fe8b55f",
+    "url": "/myBurger-react-app/static/css/main.013eb44d.chunk.css"
+  },
+  {
+    "revision": "117b420bcbe490280459",
+    "url": "/myBurger-react-app/static/js/2.7c86e8f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myBurger-react-app/static/js/2.7c86e8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6d8bdb0957073f5290",
+    "url": "/myBurger-react-app/static/js/3.5a0d73d6.chunk.js"
+  },
+  {
+    "revision": "95076e3ce860fd4f9862",
+    "url": "/myBurger-react-app/static/js/4.17e9e1eb.chunk.js"
+  },
+  {
+    "revision": "e6134b2e58ec4953b418",
+    "url": "/myBurger-react-app/static/js/5.a5178c19.chunk.js"
+  },
+  {
+    "revision": "5903dca85c33aec5a9ff",
+    "url": "/myBurger-react-app/static/js/6.e7a8343e.chunk.js"
+  },
+  {
+    "revision": "4095663e76a15fe8b55f",
+    "url": "/myBurger-react-app/static/js/main.f49b60fa.chunk.js"
+  },
+  {
+    "revision": "592d3763458cdf2524f4",
+    "url": "/myBurger-react-app/static/js/runtime-main.c86271bb.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/myBurger-react-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
